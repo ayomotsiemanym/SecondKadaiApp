d39fea0217ae9ff99d94f9af7d113f09a8fad3f4
tomoya.iwata <ayomotsiemanym@iwatatomoya-no-macbook-pro.local>
2016-12-28 09:56:44 +0000

    Kadai2を一旦保存する

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,17 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        // segueから遷移先のResultViewControllerを取得する
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        //遷移先のResultViewControllerで宣言しているxにテキストを代入して渡す
+        resultViewController.x = textField.text!
+    }
+    
+    @IBAction func unwind(segue: UIStoryboardSegue){
+        
     }
 
 
